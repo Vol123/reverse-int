62925d12214aca794de24771063bc03e853a466d
Uldzimir Haleta <vahaleta@gmai.com>
2022-06-30 14:38:40 +0300

    feat: add and change code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString();
+    let result = '';
+    for (let i = n.length - 1; i >= 0; i--)
+        if (n[i] !== '-')
+            result += n[i];
+    return result;      
 }
